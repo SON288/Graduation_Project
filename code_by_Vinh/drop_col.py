272306fcef03546a018a1c8b10aa902afaa77307
Ptvinh160504 <phamthevinh0504@gmail.com>
2025-06-08 19:07:52 +0700

    xóa không cần thiết

--- code_by_Vinh/drop_col.py
+++
@@ -1,9 +0,0 @@
-class ColumnDropper:
-    def __init__(self, df):
-        self.df = df
-
-    def drop(self, columns_to_drop):
-        """
-        Xoá các cột được chỉ định khỏi DataFrame.
-        """
-        return self.df.drop(columns=columns_to_drop, axis=1)
--- code_by_Vinh/rename_colum.py
+++
@@ -1,9 +0,0 @@
-class ColumnRenamer:
-    def __init__(self, df):
-        self.df = df
-
-    def rename_columns(self, rename_map):
-        """
-        Đổi tên cột trong DataFrame theo từ điển ánh xạ.
-        """
-        return self.df.rename(columns=rename_map)
